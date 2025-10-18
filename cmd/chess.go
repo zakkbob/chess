9a9a03d84f6a359ce974ed70a2ed313e96fab7f7
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-10-18 20:00:07 +0100

    Add basic chess game command

---
+++ cmd/chess.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zakkbob/chess"
+)
+
+func main() {
+	b := chess.NewBoard()
+
+	for {
+		fmt.Println(b.String())
+
+		var from int
+		var to int
+
+		fmt.Print("From: ")
+		fmt.Scanln(&from)
+
+		fmt.Print("To: ")
+		fmt.Scanln(&to)
+
+		b.CoordinateMove(from, to, chess.NoPromotion)
+	}
+}
