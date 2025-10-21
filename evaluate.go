26b33882a9668c88fb9780a5c0af1c5975cc02a2
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-10-21 21:31:01 +0100

    Add (bad) values per position

--- evaluate.go
+++ evaluate.go
@@ -2,7 +2,17 @@ package chess
 
 import "math/bits"
 
-func sumValues(bb uint64, values [64]int) int {
+func sumWhiteValues(bb uint64, values [64]int) int {
+	val := 0
+	for bb != 0 {
+		i := bits.TrailingZeros64(bb)
+		val += values[63-i]
+		bb &= bb - 1
+	}
+	return val
+}
+
+func sumBlackValues(bb uint64, values [64]int) int {
 	val := 0
 	for bb != 0 {
 		i := bits.TrailingZeros64(bb)
@@ -12,14 +22,69 @@ func sumValues(bb uint64, values [64]int) int {
 	return val
 }
 
+var pawnVals = [64]int{
+	00, 00, 00, 00, 00, 00, 00, 00,
+	10, 20, 20, 30, 30, 20, 20, 10,
+	0, 10, 10, 20, 20, 10, 10, 00,
+	-10, 00, 00, 10, 10, 00, 00, -10,
+	-20, -10, -10, 00, 00, -10, -10, -20,
+	-30, -20, -20, -20, -20, -20, -20, -30,
+	-40, -30, -30, -30, -30, -30, -30, -40,
+	00, 00, 00, 00, 00, 00, 00, 00,
+}
+
+var rookVals = [64]int{
+	-20, -20, -10, 00, 00, -10, -20, -20,
+	-20, -10, 00, 10, 10, 00, -10, -20,
+	-10, 00, 10, 20, 20, 10, 00, -10,
+	00, 10, 20, 30, 30, 20, 10, 00,
+	00, 10, 20, 30, 30, 20, 10, 00,
+	-10, 00, 10, 20, 20, 10, 00, -10,
+	-20, -10, 00, 10, 10, 00, -10, -20,
+	-20, -20, -10, 00, 00, -10, -20, -20,
+}
+
+var queenVals = [64]int{
+	-20, -20, -10, 00, 00, -10, -20, -20,
+	-20, -10, 00, 10, 10, 00, -10, -20,
+	-10, 00, 10, 20, 20, 10, 00, -10,
+	00, 10, 20, 30, 30, 20, 10, 00,
+	00, 10, 20, 30, 30, 20, 10, 00,
+	-10, 00, 10, 20, 20, 10, 00, -10,
+	-20, -10, 00, 10, 10, 00, -10, -20,
+	-20, -20, -10, 00, 00, -10, -20, -20,
+}
+
+var kingVals = [64]int{
+	-50, -50, -50, -50, -50, -50, -50, -50,
+	-40, -40, -50, -50, -50, -50, -40, -40,
+	-30, -30, -40, -50, -50, -40, -30, -30,
+	-20, -20, -30, -40, -40, -30, -20, -20,
+	-10, -10, -20, -30, -30, -20, -10, -10,
+	0, 0, -10, -20, -20, -10, 0, 0,
+	10, 10, 0, -10, -10, 0, 10, 10,
+	20, 20, 10, 0, 0, 10, 20, 20,
+}
+
 var knightVals = [64]int{
 	-50, -40, -30, -30, -30, -30, -40, -50,
-	-40, -20, 0, 5, 5, 0, -20, -40,
-	-30, 0, 10, 15, 15, 10, 0, -30,
+	-40, -20, 00, 5, 5, 00, -20, -40,
+	-30, 00, 10, 15, 15, 10, 00, -30,
 	-30, 5, 15, 20, 20, 15, 5, -30,
 	-30, 5, 15, 20, 20, 15, 5, -30,
-	-30, 0, 10, 15, 15, 10, 0, -30,
-	-40, -20, 0, 5, 5, 0, -20, -40,
+	-30, 00, 10, 15, 15, 10, 00, -30,
+	-40, -20, 00, 5, 5, 00, -20, -40,
+	-50, -40, -30, -30, -30, -30, -40, -50,
+}
+
+var bishopVals = [64]int{
+	-50, -40, -30, -30, -30, -30, -40, -50,
+	-40, -20, 00, 5, 5, 00, -20, -40,
+	-30, 00, 10, 15, 15, 10, 00, -30,
+	-30, 5, 15, 20, 20, 15, 5, -30,
+	-30, 5, 15, 20, 20, 15, 5, -30,
+	-30, 00, 10, 15, 15, 10, 00, -30,
+	-40, -20, 00, 5, 5, 00, -20, -40,
 	-50, -40, -30, -30, -30, -30, -40, -50,
 }
 
@@ -60,7 +125,12 @@ func Evaluate(b Board) int {
 		queenWt*(wQ-bQ) +
 		kingWt*(wK-bK)
 
-	positionalScore := sumValues(b.whiteKnights, knightVals) - sumValues(b.blackKnights, knightVals)
+	positionalScore := sumWhiteValues(b.whitePawns, pawnVals) - sumBlackValues(b.blackPawns, pawnVals) +
+		sumWhiteValues(b.whiteKnights, knightVals) - sumBlackValues(b.blackKnights, knightVals) +
+		sumWhiteValues(b.whiteRooks, rookVals) - sumBlackValues(b.blackKnights, rookVals) +
+		sumWhiteValues(b.whiteKings, kingVals) - sumBlackValues(b.blackKings, kingVals) +
+		sumWhiteValues(b.whiteBishops, bishopVals) - sumBlackValues(b.blackBishops, bishopVals) +
+		sumWhiteValues(b.whiteQueens, queenVals) - sumBlackValues(b.blackQueens, queenVals)
 
 	score := materialScore + positionalScore
 
